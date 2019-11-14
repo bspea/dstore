5116281d3795460881d6d050110b9273b5af429e
Han9005 <racy0987@naver.com>
2019-11-14 21:46:56 +0900

    첫 번째 커밋입니다. dstayproject만 onstage 되었습니다

---
+++ dstayproject/src/main/java/com/kh/dstay/member/model/service/MemberService.java
@@ -0,0 +1,8 @@
+package com.kh.dstay.member.model.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.kh.dstay.member.model.dao.MemberDao;
+
+public interface MemberService {
+}
