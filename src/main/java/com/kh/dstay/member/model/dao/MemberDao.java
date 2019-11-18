234effa08196b3c8f924f01ab3f3935fc0baf394
user2 <user2@KH_I>
2019-11-18 20:58:22 +0900

    reset!!!

--- src/main/java/com/kh/dstay/member/model/dao/MemberDao.java
+++ src/main/java/com/kh/dstay/member/model/dao/MemberDao.java
@@ -1,26 +1,5 @@
 package com.kh.dstay.member.model.dao;
 
-<<<<<<< HEAD
 public class MemberDao {
 
-=======
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
-import com.kh.dstay.member.model.vo.Member;
-
-@Repository
-public class MemberDao {
-
-	@Autowired
-	private SqlSessionTemplate sqlSession;
-	
-	public Member login(Member mem) {
-		
-		return sqlSession.selectOne("memberMapper.login",mem);
-	}
-	
-	
->>>>>>> refs/remotes/origin/jonghwan
 }
