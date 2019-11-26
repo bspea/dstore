b1f2726c002b40417a35bbdb49a636bd1c461f84
user2 <user2@KH_I>
2019-11-26 20:44:11 +0900

    mycoupon vo 추가

---
+++ dstayproject/src/main/java/com/kh/dstay/member/model/vo/MyCoupon.java
@@ -0,0 +1,18 @@
+package com.kh.dstay.member.model.vo;
+
+import java.sql.Date;
+
+public class MyCoupon {
+	
+	private int couponNo;
+	private int memberNo;
+	private int couponInfoNo;
+	private int productNo;
+	private int couponCode;
+	private String couponName;
+	private double saleRate;
+	private Date startDate;
+	private Date endDate;
+	private String status;
+
+}
